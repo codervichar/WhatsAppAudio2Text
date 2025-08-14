b9e353f0420d5ea1aa95532443527532c22fdc14
ppanchal912 <ppanchal912@gmail.com>
2025-08-14 16:35:59 +0530

    Working on whatsapp webhook

--- backend/controllers/whatsappController.js
+++ backend/controllers/whatsappController.js
@@ -118,8 +118,8 @@ const handleWhatsAppMessage = async (req, res) => {
 
     // Upload to S3
     const s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
       region: process.env.AWS_REGION,
     });
     const unique = uuidv4();
